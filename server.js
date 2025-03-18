399970e13e76964ebdbac4f7303d661305f2efc0
spmerdegia <spmerdegia@up.edu.ph>
2025-03-18 13:39:29 +0800

    feat: added a server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Handle the root route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
